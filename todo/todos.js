d6fa03aa80aa068c117ff9b75b0acf5f9623bf06
W1ll-Du <70287641+W1ll-Du@users.noreply.github.com>
2021-01-25 14:56:05 -0500

    Update todos.js

--- todo/todos.js
+++ todo/todos.js
@@ -1,1 +1,7 @@
-
+function addInput() {
+  var newItem = document.getElementById("item");
+  var li = document.createElement("li");
+  li.innerHTML = newItem.innerHTML;
+  newItem.innerHTML = "";
+  document.getElementById("list").appendChild(li);
+}
