087516a0e146cb72f3d7299911bdf6edae0bfbda
W1ll-Du <70287641+W1ll-Du@users.noreply.github.com>
2021-01-26 11:40:41 -0500

    Update todos.js

--- todo/todos.js
+++ todo/todos.js
@@ -14,7 +14,7 @@ function addInput() {
 
 var list = document.getElementById("list");
 list.addEventListener("click", function(ev) {
-  if (ev.target.tagName === "li") {
+  if (ev.target.tagName === "LI") {
     ev.target.classList.toggle("checked");
   }
 }, false);
