8a99a4514a3c6476899cf2864c7cedca9b64e513
W1ll-Du <70287641+W1ll-Du@users.noreply.github.com>
2021-01-28 10:21:20 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -1,5 +1,23 @@
 window.addEventListener("load", function() {
+  let long;
+  let lat;
+  let ft;
+  let ct;
+  let loc;
   if (navigator.geolocation) {
-    
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      
+      const api = `https://api.weatherapi.com/v1/current.json?key=9b5e4d329c724f6ead0151042212801&q=${lat},${long}`;
+      
+      fetch(api)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+       
+      })
+    });
   }
 });
